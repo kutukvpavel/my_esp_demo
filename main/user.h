686dd4853d60c78b5effa29831f465532e7769a6
kutukvpavel <41666738+kutukvpavel@users.noreply.github.com>
2022-04-25 20:27:04 +0300

    initial commit

---
+++ main/user.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
